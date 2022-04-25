6bf9fef904a6be1fd4c7cf94bd415f0712a0460e
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-26 02:19:52 +0600

    adding steps

---
+++ __server-deploy.js
@@ -0,0 +1,16 @@
+/* **
+---------------
+* One Time 
+---------------
+* 1. create heroku account
+* 2. verify email
+* 3. instal heroku cli
+* 4. heroku login
+-----------------------
+
+for each project one time
+-----------------------
+* 1. heroku create
+
+
+*/
--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({message: 'unauthorized access'})
+  } 
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({message:'forbidden access'})
+    }
+    console.log('decode', decoded);
+    req.decoded = decoded;
+    next();
+  })
+  
+  
+}
 
 // connecting data base
 
@@ -25,63 +43,72 @@ async function run() {
     const serviceCollection = client.db('geniusCar').collection('service');
     const orderCollection = client.db('geniusCar').collection('order');
 
-    //  AUTH 
-    app.get('/login', async (req, res) => {
-      
-    })
+    //  AUTH
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      });
+      res.send({ accessToken });
+    });
     //   GET : get data from database Services API
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-        const service = await cursor.toArray();
-        res.send(service);
+      const service = await cursor.toArray();
+      res.send(service);
     });
-      
-        app.get('/service/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
 
-          const service = await serviceCollection.findOne(query);
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
 
-          res.send(service);
-      });
-/* --------get end-------- */
+      const service = await serviceCollection.findOne(query);
+
+      res.send(service);
+    });
+    /* --------get end-------- */
 
     //   POST : Send data or add data in database
-      
-      app.post('/service', async (req, res) => {
-          const newService = req.body;
-          const result = await serviceCollection.insertOne(newService);
 
-          res.send(result);
-      });
+    app.post('/service', async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+
+      res.send(result);
+    });
 
     //   Delete: delete data from database
-      
-      app.delete('/service/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const result = await serviceCollection.deleteOne(query);
-          res.send(result);
 
-      })
+    app.delete('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
     // order collection API
     app.post('/order', async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
-    })
+    });
 
     // get api for order
 
-    app.get('/order', async (req, res) => {
+    app.get('/order', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
       console.log(email);
-      const query = {email:email};
-      const cursor = orderCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders)
-    })
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } 
+      else {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+    });
   } finally {
   }
 }
